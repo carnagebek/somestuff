108e9a4047107e28b31f607094919928bd4704f8
carnagebek <44438743+carnagebek@users.noreply.github.com>
2019-08-01 16:59:28 -0300

    Add files via upload

--- prometheusconfig.sh
+++ prometheusconfig.sh
@@ -4,14 +4,15 @@ useradd --no-create-home --shell /bin/false prometheus
 mkdir /etc/prometheus
 mkdir /var/lib/prometheus
 wget -P /root/ https://github.com/prometheus/prometheus/releases/download/v2.11.1/prometheus-2.11.1.linux-amd64.tar.gz
-#tar -C /root/ -xvzf /root/prometheus-2.11.1.linux-amd64.tar.gz
+tar -C /root/ -xvzf /root/prometheus-2.11.1.linux-amd64.tar.gz
 mv /root/prometheus-2.11.1.linux-amd64 /root/prometheus-files
 cp -v /root/prometheus-files/./prometheus /usr/local/bin/
 cp -v /root/prometheus-files/./promtool /usr/local/bin/
 cp -vr /root/prometheus-files/consoles /etc/prometheus
 cp -vr /root/prometheus-files/console_libraries /etc/prometheus
 cp -v /root/prometheus-files/prometheus.yml /etc/prometheus
-wget -P /etc/systemd/system https://github.com/carnagebek/somestuff/blob/master/prometheus.service
+wget wget https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl.py -O /usr/local/bin/systemctl
+wget https://raw.githubusercontent.com/carnagebek/somestuff/master/prometheus.service -O /etc/systemd/system/prometheus.service
 chown -R prometheus:prometheus /etc/prometheus
 chown -R prometheus:prometheus /var/lib/prometheus
 chown prometheus:prometheus /usr/local/bin/prometheus
