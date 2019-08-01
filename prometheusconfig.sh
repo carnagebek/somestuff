eff1c574c41bc57d6ebee07aea444d70aa2892f3
carnagebek <44438743+carnagebek@users.noreply.github.com>
2019-08-01 11:17:09 -0300

    Add files via upload

--- prometheusconfig.sh
+++ prometheusconfig.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 useradd --no-create-home --shell /bin/false prometheus
 mkdir /etc/prometheus
 mkdir /var/lib/prometheus
 wget -P /root/ https://github.com/prometheus/prometheus/releases/download/v2.11.1/prometheus-2.11.1.linux-amd64.tar.gz
-tar -C /root/ -xvzf /root/prometheus-2.11.1.linux-amd64.tar.gz
+#tar -C /root/ -xvzf /root/prometheus-2.11.1.linux-amd64.tar.gz
 mv /root/prometheus-2.11.1.linux-amd64 /root/prometheus-files
 cp -v /root/prometheus-files/./prometheus /usr/local/bin/
 cp -v /root/prometheus-files/./promtool /usr/local/bin/
